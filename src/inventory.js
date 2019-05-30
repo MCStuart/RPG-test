12561c27d88bf3bd245ea11c1611377378f2deba
Darrion Gering and Stuart McKay <darrion+stuart@>
2019-05-30 16:45:01 -0700

    Add tests for checkIfPlayerCanHold

--- src/inventory.js
+++ src/inventory.js
@@ -18,6 +18,14 @@ export class Inventory{
       }
     }
   }
+
+  checkIfPlayerCanHold(item, player) {
+    if((player.strength * 2 || player.intelligence * 2) >= item.weight) {
+      this.addItem(item);
+      return true;
+    }
+    return false;
+  }
 }
 
 export class Item {
@@ -25,11 +33,75 @@ export class Item {
     this.name = "Garbage";
     this.weight = 1;
     this.value = 0;
-    this.damage = 1;
+    this.damage = 0;
+    this.defense = 0;
     this.damageType = "Phyiscal";
+    this.weaponTier = "Wooden";
     Object.assign(this, stats);
   }
 
+  woodenItem() {
+    this.weight = 20;
+    this.value = 10;
+    this.damage = 2;
+    this.defense = 2;
+    this.weaponTier = "Wooden";
+  }
 
+  ironItem() {
+    this.weight = 30;
+    this.value = 20;
+    this.damage = 5;
+    this.defense = 5;
+    this.weaponTier = "Iron";
+  }
+
+  steelItem() {
+    this.weight = 40;
+    this.value = 30;
+    this.damage = 10;
+    this.defense = 10;
+    this.weaponTier = "Steel";
+  }
+
+  mithrilItem() {
+    this.weight = 50;
+    this.value = 40;
+    this.damage = 20;
+    this.defense = 20;
+    this.weaponTier = "Mithril";
+  }
+
+  obsidianItem() {
+    this.weight = 70;
+    this.value = 50;
+    this.damage = 30;
+    this.defense = 30;
+    this.weaponTier = "Obsidian";
+  }
+
+  dragonBoneItem() {
+    this.weight = 100;
+    this.value = 60;
+    this.damage = 40;
+    this.defense = 40;
+    this.weaponTier = "Dragon Bone";
+  }
+
+  getWeaponTier() {
+    if(this.weaponTier === "Wooden") {
+      this.woodenItem();
+    } if(this.weaponTier === "Iron") {
+      this.ironItem();
+    } if(this.weaponTier === "Steel") {
+      this.steelItem();
+    } if(this.weaponTier === "Mithril") {
+      this.mithrilItem();
+    } if(this.weaponTier === "Obsidian") {
+      this.obsidianItem();
+    } if(this.weaponTier === "Dragon Bone") {
+      this.dragonBoneItem();
+    }
+  }
 }
 
--- test/inventory.test.js
+++ test/inventory.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { setBrowserVariable, getBrowserVariable } from '../src/functions';
 import { Character, Enemy } from '../src/character';
 import { Combat } from '../src/battle';
@@ -11,26 +12,41 @@ describe('Inventory', function () {
       inventory: new Inventory([
         {name: "Sword", weight: 12}, 
         {name: "Shield", weight: 20}])
-      });
     });
-    it('inventory constructor', function(){
-      expect(knight.inventory.items[0].name).toEqual("Sword");
-      expect(knight.inventory.items[0].damageType).toEqual("Phyiscal");
-    });
-    
-    it('inventory add item', function(){
-      knight.inventory.addItem({name: "Sword", weight:12, value:0, damage:1, damageType: "Physical"})
-      expect(knight.inventory.items[0].name).toEqual("Sword");
-      expect(knight.inventory.items[0].damageType).toEqual("Phyiscal");
-    });
-
-    it("inventory remove item", function() {
-      knight.inventory.addItem({name: "Sword", weight:12, value:0, damage:1, damageType: "Physical"})
-      expect(knight.inventory.items[0].name).toEqual("Sword");
-      knight.inventory.removeItem("Shield");
-      expect(knight.inventory.items.length).toEqual(2);
-    })
   });
+  it('inventory constructor', function(){
+    expect(knight.inventory.items[0].name).toEqual("Sword");
+    expect(knight.inventory.items[0].damageType).toEqual("Phyiscal");
+  });
+  
+  it('inventory add item', function(){
+    knight.inventory.addItem({name: "Sword", weight:12, value:0, damage:1, damageType: "Physical"})
+    expect(knight.inventory.items[0].name).toEqual("Sword");
+    expect(knight.inventory.items[0].damageType).toEqual("Phyiscal");
+  });
+
+  it("inventory remove item", function() {
+    knight.inventory.addItem({name: "Sword", weight:12, value:0, damage:1, damageType: "Physical"})
+    expect(knight.inventory.items[0].name).toEqual("Sword");
+    knight.inventory.removeItem("Shield");
+    expect(knight.inventory.items.length).toEqual(2);
+  })
+
+  //after implementing checkIfPlayerCanHold()
+  it('player gets an item they can hold', function() {
+    let woodenSword = new Item({
+      name: "Wooden Sword",
+      weaponTier: "Wooden",
+    });
+    woodenSword.getWeaponTier();
+    knight.inventory.checkIfPlayerCanHold(woodenSword, knight);
+    expect(knight.inventory.items.length).toEqual(3);
+  });
+
+  it('player gets a melee item they cant use', function() {
+    knight.inventory.addItem({name: "Dragon Bone Spear", weaponTier: "Dragon Bone"});
+  });
+});
     
 describe('Item', function() {
   it('creates a new item', function() {
@@ -39,5 +55,18 @@ describe('Item', function() {
     });
     expect(ironArmor.name).toEqual("Iron Armor");
   });
+
+  it('gets values of an item based on the item tier', function() {
+    let treeBranch = new Item({
+      name: "Tree Branch", weight: 40, value: 10, weaponTier: "Wooden"
+    });
+    treeBranch.getWeaponTier();
+    expect(treeBranch.damage).toEqual(2);
+    expect(treeBranch.defense).toEqual(2);
+    expect(treeBranch.weight).toEqual(20);
+    expect(treeBranch.weaponTier).toEqual("Wooden");
+    expect(treeBranch.value).toEqual(10);
+  });
+
 });
 
